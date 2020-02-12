0aad02b90d06dce3849ccdd39d3f8f1e95df0af0
davidrharvey <david.harvey@mymail.champlain.edu>
2020-02-12 01:25:31 -0500

    Updated New Task Page
    Functionality not yet implemented

--- src/screens/add-task/index.js
+++ src/screens/add-task/index.js
@@ -1,14 +1,41 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import PageLayout from "../../components/PageLayout";
+import Button from "@material-ui/core/Button";
+import {Link} from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Input from "@material-ui/core/Input";
 
 const AddTask = () => {
+    function newTask(e){
 
+    }
     return (
         <PageLayout>
-            <Typography variant={'h2'}>
-                {'Add New Task:'}
-            </Typography>
+
+            <Grid container justify={'center'}>
+                <Typography variant={'h2'}>
+                    {'New Task:'}
+                </Typography>
+
+                <Grid container item xs={12} justify={'center'}>
+                    <form titleForm autoComplete="off">
+                        <Input variant={'outlined'} placeholder="Title " inputProps={{ 'aria-label': 'description' }} />
+                    </form>
+                </Grid>
+
+                <Grid container item xs={12} justify={'center'}>
+                    <form descForm autoComplete="off">
+                        <Input variant={'outlined'} placeholder="Task Description" inputProps={{ 'aria-label': 'description' }} />
+                    </form>
+                </Grid>
+                <Grid container item xs={12} justify={'center'}>
+                    <Button component={Link} variant={'outlined'}>
+                        {'Create'}
+                    </Button>
+                </Grid>
+            </Grid>
+
         </PageLayout>
     )
 };
